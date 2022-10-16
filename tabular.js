6ef3c722afaed48699c34b55ca99029cf6011e23
mar1boroman <omkarkonnur91@gmail.com>
2022-10-16 23:34:22 +0530

    Redid the table construction

--- tabular.js
+++ tabular.js
@@ -51,6 +51,60 @@ $(document).ready(function () {
     },
   });
 
+  const fileUrl = "Redis_Commands_Cheatsheet_ListExamples.csv";
+  Papa.parse(fileUrl, {
+    download: true,
+    complete: function (result) {
+      tbl = result.data;
+      tbl.forEach((r) => {
+        let row = document.createElement("tr");
+
+        s = r.slice(0, -1);
+
+        category_format = s[0];
+        if (category_format == "BF") {
+          category_format = "Bloom Filters";
+        } else if (category_format == "CF") {
+          category_format = "Cuckoo Filters";
+        } else if (category_format == "CMS") {
+          category_format = "Count-Min Sketch";
+        }
+
+        complexity = s[4];
+        if (complexity == "") {
+          complexity = "Check Documentation";
+        } else if (complexity == "N/A") {
+          complexity = "Check Documentation";
+        }
+
+        tx_list = {
+          Category: category_format, // Category
+          Command: '<a href="' + s[3] + '" target = "_blank">' + s[1] + "</a>", // Command with link
+          Description: s[2], // Description
+          "Time Complexity": complexity, // Time Complexity
+          Availability: s[5], // Availability
+          "ACL Categories": s[6], // ACL Category
+          //   s[7], // Command Syntax
+        };
+
+        command_str = "";
+        var cmd_list = [];
+        r.slice(-1).forEach((e) => {
+          if (JSON.parse(e)) {
+            JSON.parse(e).forEach((c) => {
+              command_str += c + "<br>";
+              cmd_list.push(c);
+            });
+          }
+        });
+
+        r = table.row.add(tx_list).draw().node();
+        r.setAttribute("examples-list", cmd_list);
+        r.setAttribute("command-syntax", s[7]);
+      });
+    },
+  });
+
   function render_cli_with_examples(list, id) {
     pre_command_list = "";
     list.split(",").forEach((ex) => {
@@ -89,6 +143,7 @@ $(document).ready(function () {
   // Add event listener for opening and closing details
   $("#table-cheatsheet tbody").on("click", "td.dt-control", function () {
     var tr = $(this).closest("tr");
+
     var row = table.row(tr);
     var examples_list = tr.attr("examples-list");
     var command_syntax = tr.attr("command-syntax");
